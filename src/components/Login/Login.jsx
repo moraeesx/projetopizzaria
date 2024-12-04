ad2330d3b3ae8882d7abb5a5139fbddac731c068
moraeesx <luizgustavogms9@gmail.com>
2024-12-03 22:14:38 -0300

    logicalogin

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"; // Importa o hook de navegação
 import styles from "./Login.module.css";
+import users from "./users.json"; // Importa o JSON com os dados dos usuários
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate(); // Instancia o hook de navegação
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    // Verificar se as credenciais estão no JSON
+    const user = users.find(
+      (user) => user.email === email && user.senha === senha
+    );
+
+    if (user) {
+      navigate("/Home"); // Redireciona para a página Home
+    } else {
+      setError("Email ou senha inválidos.");
+    }
+  };
+
   return (
     <div className={styles.loginBd}>
       <header>
@@ -16,7 +39,7 @@ const Login = () => {
       </header>
       <main>
         <section aria-label="Login">
-          <form action="./home.html" className={styles.login}>
+          <form onSubmit={handleLogin} className={styles.login}>
             <div className={styles.loginEscopo}>
               <label htmlFor="email" className={styles.loginTexto}>
                 Email
@@ -26,6 +49,8 @@ const Login = () => {
                 id="email"
                 name="email"
                 className={styles.loginEmail}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required
               />
               <label htmlFor="senha" className={styles.loginTexto}>
@@ -36,10 +61,15 @@ const Login = () => {
                 id="senha"
                 name="senha"
                 className={styles.loginEmail}
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
                 required
               />
+              {error && <p className={styles.error}>{error}</p>}
               <div className={styles.loginButton}>
-                <button className={styles.buttonLogin}>Entrar</button>
+                <button type="submit" className={styles.buttonLogin}>
+                  Entrar
+                </button>
               </div>
               <a className={styles.buttonText} href="./">
                 Esqueceu sua senha?
